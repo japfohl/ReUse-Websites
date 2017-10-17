4734bf2fc0786062d4cf3ac8ad5df6abfd77ea25
Joe Pfohl <joepfohl@gmail.com>
2017-10-16 21:31:23 -0400

    simplify index and fix errors from autoload improperly setup

--- public_html/index.php
+++ public_html/index.php
@@ -1,69 +1,28 @@
 <?php
-	
-	//For DEBUGGING	
-	ini_set('display_errors', 'On');	
 
-		
-	/**************************************************************************
-	*				Requirements
-	**************************************************************************/
-	
-	require_once '../vendor/autoload.php';
+// autoload everything
+require_once dirname(__FILE__).'/../vendor/autoload.php';
 
-	//functions to facilitate connection to reuse database
-		// - connectReuseDB()		<-- Create mysqli object using reuse db creds
+//For DEBUGGING
+ini_set('display_errors', 'On'); // TODO: turn of in production
 
-    require '../App/Database/reuseConnect.php';
+/**************************************************************************
+*				Routing set up
+***************************************************************************/
+$app = new \Slim\Slim(
+    //More debugging
+    array( 'debug' => true )
+);
 
-	//functions facilitating XML file generation for mobile
-		// - reuse_generateXML()	<-- Produces XML file
-		// - echoXMLFile()		<-- Echos file after generation
-	require '../App/Lib/xmlGenerator/xmlGenerator.php';
-	
-	//functions facilitating Bing Geocoder
-	require '../App/Lib/BingGeocoder/geocoder.php';
+foreach (glob('../App/Api/*.php') as $routeFile) {
+        require $routeFile;
+}
 
-	/**************************************************************************
-	*				Routing set up
-	***************************************************************************/
-	$app = new \Slim\Slim(
-		//More debugging
-		array( 'debug' => true )
-    );
+// TODO: determine if we actually need to set the content type here
+$app->response->headers->set('Content-Type', 'application/json');
 
-    $routeFiles = (array) glob('../App/Api/*.php');
-    foreach($routeFiles as $routeFile) {
-            require $routeFile;
-    }
+$app->get('/hello/:name', function ($name) {
+    echo "Hello, $name";
+});
 
-    
-    $app->response->headers->set('Content-Type', 'application/json');
-
-    $app->get('/hello/:name', function ($name) {
-    	echo "Hello, $name";
-    });
-    
-    $app->get('/test/:cid', function ($cid) use ($app) {
-		// query for items with category $cid
-		$items = ItemQuery::create()
-			->filterByCategory($cid)
-			->find();
-		
-		// set our headers	
-		$app->response->headers->set('Content-Type', 'text/html');
-		
-		// echo start of the html
-		echo '<html><body><ul>';
-		
-		// iterate over each thing returned form the db
-		foreach ($items as $item)
-		{
-			echo '<li>'.$item->getName().'</li>';
-		}
-		
-		// close out the html
-		echo '</ul></body></html>';
-    });
-    
-	$app->run();	
-?>
+$app->run();
