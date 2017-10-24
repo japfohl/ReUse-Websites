49a46306d7b4a2f4f5fafa3de1208a12e5229655
Joe Pfohl <joepfohl@gmail.com>
2017-10-23 23:28:52 -0400

    Update phone routes tests to use assertions and xpath

--- test/tests/PhoneRoutesTest.php
+++ test/tests/PhoneRoutesTest.php
@@ -5,19 +5,19 @@ require_once dirname(__FILE__).'/../../vendor/autoload.php';
 
 use PHPUnit\Framework\TestCase;
 
-final class PhoneRoutesTest extends TestCase
-{
+final class PhoneRoutesTest extends TestCase {
+
     // class level variables accessible by each test being run
     protected $client;
 
     // create a new Guzzle client before running each test
-    protected function setUp()
-    {
+    protected function setUp() {
+
         $this->client = new GuzzleHttp\Client(['base_uri' => getenv('API_ADDR')]);
     }
 
-    public function testWholeReuseDatabaseRoute()
-    {
+    public function testWholeReuseDatabaseRoute() {
+
         // send the request to the route and store the response
         $response = $this->client->request('GET', '/reuseDB');
 
@@ -27,8 +27,8 @@ final class PhoneRoutesTest extends TestCase
         $this->reUseDbXmlIsValid($response);
     }
 
-    public function testRecyclingCenterNamesOnlyListRoute()
-    {
+    public function testRecyclingCenterNamesOnlyListRoute() {
+
         // send the request to the route and store the response
         $response = $this->client->request('GET', '/recycleNameXML');
 
@@ -46,10 +46,10 @@ final class PhoneRoutesTest extends TestCase
         $recycleCenterNames = $xml->children()[0];
         $this->assertEquals('recycle_center_names', $recycleCenterNames->getName());
 
-        if ($recycleCenterNames->count() > 0)
-        {
-            foreach ($recycleCenterNames->children() as $name)
-            {
+        if ($recycleCenterNames->count() > 0) {
+
+            foreach ($recycleCenterNames->children() as $name) {
+
                 // validate tag name
                 $this->assertEquals('name', $name->getName());
 
@@ -59,8 +59,8 @@ final class PhoneRoutesTest extends TestCase
         }
     }
 
-    public function testRecyclingCenterListIncludingAllRelevantDataRoute()
-    {
+	public function testRecyclingCenterListIncludingAllRelevantDataRoute() {
+
         // send the request to the route and store the response
         $response = $this->client->request('GET', '/recycleXML');
 
@@ -84,8 +84,8 @@ final class PhoneRoutesTest extends TestCase
         $this->assertGreaterThanOrEqual(1, $recycleList->count());
 
         // loop over and validate each child of recycle list
-        foreach ($recycleList->children() as $business)
-        {
+        foreach ($recycleList->children() as $business) {
+
             // validate name and child count
             $this->assertEquals('business', $business->getName());
             $this->assertEquals(5, $business->count());
@@ -153,10 +153,9 @@ final class PhoneRoutesTest extends TestCase
             $this->assertEquals('link_list', $linkList->getName());
 
             // validate each link in link list
-            if ($linkList->count() > 0)
-            {
-                foreach ($linkList->children() as $link)
-                {
+            if ($linkList->count() > 0) {
+                foreach ($linkList->children() as $link) {
+
                     $this->assertEquals('link', $link->getName());
                     $this->assertEquals(2, $link->count());
                     $this->assertEquals('name', $link->children()[0]->getName());
@@ -168,8 +167,8 @@ final class PhoneRoutesTest extends TestCase
         }
     }
 
-    public function testDonorAndSponsorInformationListRoute()
-    {
+    public function testDonorAndSponsorInformationListRoute() {
+
         // send the request to the route and store the response
         $response = $this->client->request('GET', '/donorXML');
 
@@ -187,10 +186,9 @@ final class PhoneRoutesTest extends TestCase
         $donorList = $xml->children()[0];
         $this->assertEquals('donor_list', $donorList->getName());
 
-        if ($donorList->count() > 0)
-        {
-            foreach ($donorList->children(0) as $donor)
-            {
+        if ($donorList->count() > 0) {
+            foreach ($donorList->children(0) as $donor) {
+
                 // TODO: get donor spec and add validation code here
             }
         }
@@ -198,8 +196,8 @@ final class PhoneRoutesTest extends TestCase
 
     // HELPER FUNCTIONS
 
-    private function reUseDbXmlIsValid($res)
-    {
+    private function reUseDbXmlIsValid($res) {
+
         // convert the response body to an XML element
         $xml = new SimpleXMLElement((string) $res->getBody());
 
@@ -208,70 +206,158 @@ final class PhoneRoutesTest extends TestCase
         );
 
         $this->assertEquals(2, $xml->count());
-        
-        $revision = $xml->xpath('./Revision')[0];
+
+        $revision = $xml->xpath('./Revision');
         $this->assertNotFalse($revision);
-        $this->assertEquals('Revision', $revision->getName());
+        $this->assertEquals('Revision', $revision[0]->getName());
 
         // get the business list
-        $businessList = $xml->xpath('./BusinessList')[0];
+        $businessList = $xml->xpath('./BusinessList');
         $this->assertNotFalse($businessList);
-
-        // verify tag name
+        $businessList = $businessList[0];
         $this->assertEquals('BusinessList', $businessList->getName());
-
-        // verify tag count
         $this->assertGreaterThanOrEqual(1, $businessList->count());
-        
-            // validate each business in the business list
-        foreach ($businessList->children() as $business)
-        {
-            // validate tag name
+
+        // validate each business in the business list
+        foreach ($businessList->children() as $business) {
+
+            // validate business
             $this->assertEquals('business', $business->getName());
-            
-            // verify child count
             $this->assertEquals(5, $business->count());
-            
-            // get id tag
-            $id = $business->xpath('./id')[0];
+
+            // validate id
+            $id = $business->xpath('./id');
             $this->assertNotFalse($id);
+            $id = $id[0];
+            $this->assertEquals('id', $id[0]->getName());
+            $this->assertEquals(0, $id[0]->count());
 
-            // verify tag name
-            $this->assertEquals('id', $id->getName());
-            
-            // verify child count
-            $this->assertEquals(0, $id->count());
-
-            // get name tag
-            $name = $business->xpath('./name')[0];
+            // validate name
+            $name = $business->xpath('./name');
             $this->assertNotFalse($name);
-
-            // verify tag name
-            $this->assertEquals('name', $name->getName());
-
-            // verify child count
-            $this->assertEquals(0, $name->count());
+            $name = $name[0];
+            $this->assertEquals('name', $name[0]->getName());
+            $this->assertEquals(0, $name[0]->count());
 
             // get contact info tage
-            $contactInfo = $business->xpath('contact_info')[0];
+            $contactInfo = $business->xpath('./contact_info');
             $this->assertNotFalse($contactInfo);
+            $contactInfo = $contactInfo[0];
             $this->assertEquals('contact_info', $contactInfo->getName());
             $this->assertEquals(4, $contactInfo->count());
 
-            // TODO: validate <contact_info> tag structure using xpath
+            $phone = $contactInfo->xpath('./phone');
+            $this->assertNotFalse($phone);
+            $phone = $phone[0];
+            $this->assertEquals('phone', $phone->getName());
+            $this->assertEquals(0, $phone->count());
 
-            // TODO: validate <category_list> tag structure using xpath
+            $website = $contactInfo->xpath('./website');
+            $this->assertNotFalse($website);
+            $website = $website[0];
+            $this->assertEquals('website', $website->getName());
+            $this->assertEquals(0, $website->count());
 
-            // TODO: validate <link_list> tag structure using xpath
+            $address = $contactInfo->xpath('./address');
+            $this->assertNotFalse($address);
+            $address = $address[0];
+            $this->assertEquals('address', $address->getName());
+            $this->assertEquals(5, $address->count());
+
+            $line1 = $address->xpath('./address_line_1');
+            $this->assertNotFalse($line1);
+            $line1 = $line1[0];
+            $this->assertEquals('address_line_1', $line1->getName());
+            $this->assertEquals(0, $line1->count());
+
+            $line2 = $address->xpath('./address_line_2');
+            $this->assertNotFalse($line2);
+            $line2 = $line2[0];
+            $this->assertEquals('address_line_2', $line2->getName());
+            $this->assertEquals(0, $line2->count());
+
+            $city = $address->xpath('./city');
+            $this->assertNotFalse($city);
+            $city = $city[0];
+            $this->assertEquals('city', $city->getName());
+            $this->assertEquals(0, $city->count());
+
+            $state = $address->xpath('./state');
+            $this->assertNotFalse($state);
+            $state = $state[0];
+            $this->assertEquals('state', $state->getName());
+            $this->assertEquals(0, $state->count());
+
+            $zip = $address->xpath('./zip');
+            $this->assertNotFalse($zip);
+            $zip = $zip[0];
+            $this->assertEquals('zip', $zip->getName());
+            $this->assertEquals(0, $zip->count());
+
+            $latlong = $contactInfo->xpath('./latlong');
+            $this->assertNotFalse($latlong);
+            $latlong = $latlong[0];
+            $this->assertEquals('latlong', $latlong->getName());
+            $this->assertEquals(2, $latlong->count());
+
+            $lat = $latlong->xpath('./latitude');
+            $this->assertNotFalse($lat);
+            $lat = $lat[0];
+            $this->assertEquals('latitude', $lat->getName());
+            $this->assertEquals(0, $lat->count());
+
+            $long = $latlong->xpath('./longitude');
+            $this->assertNotFalse($long);
+            $long = $long[0];
+            $this->assertEquals('longitude', $long->getName());
+            $this->assertEquals(0, $long->count());
+
+            $catList = $business->xpath('./category_list');
+            $this->assertNotFalse($catList);
+            $catList = $catList[0];
+
+            foreach ($catList->children() as $category) {
+
+				$this->assertEquals('category', $category->getName());
+				$this->assertEquals(2, $category->count());
+
+				$catName = $category->xpath('./name');
+				$this->assertNotFalse($catName);
+				$catName = $catName[0];
+				$this->assertEquals('name', $catName->getName());
+				$this->assertEquals(0, $catName->count());
+
+				$subCatList = $category->xpath('./subcategory_list');
+				$this->assertNotFalse($subCatList);
+				$subCatList = $subCatList[0];
+				$this->assertEquals('subcategory_list', $subCatList->getName());
+
+				foreach ($subCatList->children() as $subcat) {
+
+					$this->assertEquals('subcategory', $subcat->getName());
+					$this->assertEquals(0, $subcat->count());
+				}
+            }
+
+            $linkList = $business->xpath('./link_list');
+            $this->assertNotFalse($linkList);
+            $linkList = $linkList[0];
+
+            foreach ($linkList->children() as $link) {
+
+				$linkName = $link->xpath('./name');
+				$this->assertNotFalse($linkName);
+				$linkName = $linkName[0];
+				$this->assertEquals('name', $linkName->getName());
+				$this->assertEquals(0, $linkName->count());
+
+				$uri = $link->xpath('./URI');
+				$this->assertNotFalse($uri);
+				$uri = $uri[0];
+				$this->assertEquals('URI', $uri->getName());
+				$this->assertEquals(0, $uri->count());
+            }
         }
-    }
-
-    private function _Fail($msg)
-    {
-        return array(
-            "res" => false,
-            "msg" => $msg
-        );
     }
 
     private function validateGoodRequest($response)
