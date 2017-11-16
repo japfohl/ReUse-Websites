9917372273e99694626692190497b38d4c32705a
Joe Pfohl <joepfohl@gmail.com>
2017-11-16 14:49:56 -0500

    fix sessions not properly setting and unsetting

--- public_html/admin/main.php
+++ public_html/admin/main.php
@@ -36,36 +36,39 @@ include("checkSession.php");
 <!-- Main container -->
 <div class="container-fluid" id="smallCont">
     <div class="row">
-      <div class="col-xs-12 col-md-12">
-        <br/>
-        <h3>Database Management Options</h3>
-        <hr/>
-        <form id="decide">
-        <div class="form-group">
-          <label>Choose Type: </label>
-          <select class="form-control" id="choose">
-            <option value="Business">Business</option>
-            <option value="Category">Category</option>
-            <option value="Item">Items</option>
-          </select>
-        </div>
-          <div class="col-xs-12 col-md-6">
-            <!-- Button links to appropriate sites-->
-             <button Id ="submit" type ="submit" class="btn btn-default btn-lg" onclick="addRoute(); return false" align="center">Add to Selected</button>
-          </div>
-          <div class="col-xs-12 col-md-6">
-             <button Id ="submit" type ="submit" class="btn btn-default btn-lg" onclick="EditRoute(); return false" align="center">Modify or Delete From Selected</button>
-          </div>
+        <div class="col-xs-12 col-md-12">
+            <br/>
+            <h3>Database Management Options</h3>
+            <hr/>
+            <form id="decide">
+                <div class="form-group">
+                    <label>Choose Type: </label>
+                    <select class="form-control" id="choose">
+                        <option value="Business">Business</option>
+                        <option value="Category">Category</option>
+                        <option value="Item">Items</option>
+                    </select>
+                </div>
+                <div class="col-xs-12 col-md-6">
+                    <!-- Button links to appropriate sites-->
+                    <button Id="submit" type="submit" class="btn btn-default btn-lg" onclick="addRoute(); return false"
+                            align="center">Add to Selected
+                    </button>
+                </div>
+                <div class="col-xs-12 col-md-6">
+                    <button Id="submit" type="submit" class="btn btn-default btn-lg" onclick="EditRoute(); return false"
+                            align="center">Modify or Delete From Selected
+                    </button>
+                </div>
         </div> <!-- end form group -->
-      </form>
-      </div> <!-- end form -->
-      <div class="col-xs-12 col-md-12">
+        </form>
+    </div> <!-- end form -->
+    <div class="col-xs-12 col-md-12">
         <hr/>
-          <div class="filler"></div>
-      </div>
-  </div> <!-- end container-->
+        <div class="filler"></div>
+    </div>
+</div> <!-- end container-->
 
-  <script src="../js/jquery-1.11.1.min.js"></script>
-  <script src="../js/bootstrap.min.js"></script>
-  </body>
+<script src="../js/bootstrap.min.js"></script>
+</body>
 </html>
